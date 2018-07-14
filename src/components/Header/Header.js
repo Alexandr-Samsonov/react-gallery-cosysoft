341d88e5c5c3895388bb0d5c28aade0716c52cb7
Alexandr Samsonov <a.d.samsonov@yandex.ru>
2018-07-14 12:55:21 +0400

    всплытие/скрытие модального окна с формой добавления картинки

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,23 +5,47 @@ import '../../scaffolding.css';
 import './Header.css';
 
 class Header extends Component {
-  showPopupAddImage() {
-    setState;
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+    this.showPopupAddImage = this.showPopupAddImage.bind(this);
+    this.removePopupAddImage = this.removePopupAddImage.bind(this);
+    this.removeEscPopupAddImage = this.removeEscPopupAddImage.bind(this);
   }
 
-  removePopupAddImage() {
-    setState
+  showPopupAddImage() {
+    this.setState({
+      isOpen: true
+    });
+  }
+
+  removePopupAddImage(e) {
+    if (e.target.className === 'popup-add-image') {
+      this.setState({
+        isOpen: false
+      })
+    }
+  }
+
+  removeEscPopupAddImage(e) {
+    if (e.keyCode === 27) {
+      this.setState({
+        isOpen: false
+      })
+    }
   }
 
   render() {
+    let { isOpen } = this.state;
     return (
       <header className="page-header">
         <div className="page-header__wrapper">
           <h1 className="page-header__title">Gallery Images</h1>
           <a className="page-header__add-image" onClick={this.showPopupAddImage} href="#" title="Добавить изображение в галерею">Добавить IMG</a>
         </div>
-        <PopupAddImage className={ isModal ? "popup-add-image" : "popup-add-image  popup-add-image--hidden"} />
+        <PopupAddImage isOpen={isOpen} isRemove={this.removePopupAddImage} isEscRemove={this.removeEscPopupAddImage} />
       </header>
     );
   }
--- src/components/PopupAddImage/PopupAddImage.js
+++ src/components/PopupAddImage/PopupAddImage.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import '../../scaffolding.css';
 import './PopupAddImage.css';
 
 class PopupAddImage extends Component {
+  static propTypes = {
+    isOpen: PropTypes.bool,
+    isRemove: PropTypes.func,
+    isEscRemove: PropTypes.func
+  };
+
+  static defaultProps = {
+    isOpen: false,
+    isRemove: f => f,
+    isEscRemove: f => f
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.props.isEscRemove, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.props.isEscRemove, false);
+  }
+
   render() {
+    let { isOpen, isRemove } = this.props;
     return(
-      <section className={ this.props.className }>
+      <section className={ !isOpen ? "popup-add-image  popup-add-image--hidden" : "popup-add-image"} onClick={isRemove} >
         <div className="popup-add-image__description">
           <h2 className="popup-add-image__title">Добавить изображение</h2>
           <form className="popup-add-image__form" name="form">
